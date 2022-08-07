4f6f01ecd1e8a2d1c98c1f162ebe8b8e258b5f29
Milton Chandradas <milton.chandradas@live.com>
2022-08-07 11:11:10 -0400

    initial commit...

---
+++ srv/remote-service.js
@@ -0,0 +1,34 @@
+const cds = require("@sap/cds");
+
+module.exports = async (srv) => {
+   const { Employees, Orders } = srv.entities;
+
+   // connect to remote service
+   const northwindService = await cds.connect.to("northwind");
+
+   srv.on("READ", Employees, async (req, next) => {
+      if (!req.query.SELECT.columns) return await next();
+
+      const expandIndex = req.query.SELECT.columns.findIndex(
+         ({ expand, ref }) => expand && ref[0] === "orders"
+      );
+
+      if (expandIndex < 0) return await next();
+
+      try {
+         let employees = await next();
+         employees = Array.isArray(employees) ? employees : [employees];
+
+         await Promise.all(
+            employees.map(async (employee) => {
+               const orders = await northwindService.run(
+                  SELECT.from(Orders).where({ EmployeeID: employee.id })
+               );
+               employee.orders_id = orders;
+            })
+         );
+      } catch (error) {
+         console.log("Error: ", error);
+      }
+   });
+};
